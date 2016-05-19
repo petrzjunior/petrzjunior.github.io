3348da66a554d954beed5cbc006f17333db6e2e4
petrzjunior <junior@zahradnik.cz>
2016-05-19 18:52:05 +0200

    Fancybox čeština

--- informatika/js/jquery.fancybox.js
+++ informatika/js/jquery.fancybox.js
@@ -1,4 +1,4 @@
-/*!
+﻿/*!
  * fancyBox - jQuery Plugin
  * version: 2.1.5 (Fri, 14 Jun 2013)
  * @requires jQuery v1.6 or later
@@ -140,10 +140,10 @@
 				wrap     : '<div class="fancybox-wrap" tabIndex="-1"><div class="fancybox-skin"><div class="fancybox-outer"><div class="fancybox-inner"></div></div></div></div>',
 				image    : '<img class="fancybox-image" src="{href}" alt="" />',
 				iframe   : '<iframe id="fancybox-frame{rnd}" name="fancybox-frame{rnd}" class="fancybox-iframe" frameborder="0" vspace="0" hspace="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen' + (IE ? ' allowtransparency="true"' : '') + '></iframe>',
-				error    : '<p class="fancybox-error">The requested content cannot be loaded.<br/>Please try again later.</p>',
-				closeBtn : '<a title="Close" class="fancybox-item fancybox-close" href="javascript:;"></a>',
-				next     : '<a title="Next" class="fancybox-nav fancybox-next" href="javascript:;"><span></span></a>',
-				prev     : '<a title="Previous" class="fancybox-nav fancybox-prev" href="javascript:;"><span></span></a>'
+				error    : '<p class="fancybox-error">Požadovaný obrázek není dostupný.<br/>Zkuste to později.</p>',
+				closeBtn : '<a title="Zavřít" class="fancybox-item fancybox-close" href="javascript:;"></a>',
+				next     : '<a title="Další" class="fancybox-nav fancybox-next" href="javascript:;"><span></span></a>',
+				prev     : '<a title="Předchozí" class="fancybox-nav fancybox-prev" href="javascript:;"><span></span></a>'
 			},
 
 			// Properties for each animation type
