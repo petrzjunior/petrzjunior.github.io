a217e4b7d37c26e4c03e8cbda3585caf32944f9b
petrzjunior <junior@zahradnik.cz>
2016-06-10 19:28:20 +0200

    Poznávačka v1

--- poznavacka/poznavacka.js
+++ poznavacka/poznavacka.js
@@ -1,8 +1,22 @@
+var seznamKytek;
 function nahodnyObrazek() {
+  var nahoda = Math.floor((Math.random() * 50) + 1); 
+  var nazev = seznamKytek.data[nahoda][0];
+  var adresa = seznamKytek.data[nahoda][1];
   $(".reseni").removeClass("click");
-  $("#obrazek").attr("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Creeping_butercup_close_800.jpg/258px-Creeping_butercup_close_800.jpg");
-  $(".reseni-text").html("Kytka");
+  $("#obrazek").attr("src", adresa);
+  $(".reseni-text").html(nazev);
+}
+function parseCSV() {
+  Papa.parse("kytky.csv", {
+    download: true,
+    encoding: "UTF-8",
+    complete: function (results) {
+      seznamKytek = results;
+      console.log(seznamKytek);
+    }
+  });
 }
 $('.reseni').click(function () {
-    $(".reseni").toggleClass("click");
+  $(".reseni").toggleClass("click");
 });
