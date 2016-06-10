3417f4707f0ea650e8c79aa217c369f9ea22645a
petrzjunior <junior@zahradnik.cz>
2016-06-10 20:28:03 +0200

    Progress bar

--- poznavacka/poznavacka.js
+++ poznavacka/poznavacka.js
@@ -1,21 +1,38 @@
 var seznamKytek;
+var index = 0;
 function nahodnyObrazek() {
-  var nahoda = Math.floor((Math.random() * 50) + 1); 
-  var nazev = seznamKytek.data[nahoda][0];
-  var adresa = seznamKytek.data[nahoda][1];
+  var nazev = seznamKytek[index][0];
+  var adresa = seznamKytek[index][1];
   $(".reseni").removeClass("click");
   $("#obrazek").attr("src", adresa);
   $(".reseni-text").html(nazev);
+  index++;
+  if(index >= 50) {
+    seznamKytek = shuffle(seznamKytek);
+    index = 0;
+  }
+  $("#kytky-progress").css("width", index + "%");
 }
 function parseCSV() {
   Papa.parse("kytky.csv", {
     download: true,
     encoding: "UTF-8",
     complete: function (results) {
-      seznamKytek = results;
-      console.log(seznamKytek);
+      seznamKytek = results.data;
+      seznamKytek = shuffle(seznamKytek);
     }
   });
+}
+function shuffle(array) {
+    let counter = array.length;
+    while (counter > 0) {
+        let index = Math.floor(Math.random() * counter);
+        counter--;
+        let temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+    return array;
 }
 $('.reseni').click(function () {
   $(".reseni").toggleClass("click");
