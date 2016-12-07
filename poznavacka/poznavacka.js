f784d6ec09f2c028ff99ebf41cd8d16e64c507f7
Petr Zahradník <junior@zahradnik.cz>
2016-12-07 19:10:02 +0100

    Poznavacka design rework

--- poznavacka/poznavacka.js
+++ poznavacka/poznavacka.js
@@ -1,32 +1,53 @@
-﻿var seznamKytek;
+﻿
+var seznamKytek;
 var index = 0;
+
 function nahodnyObrazek() {
-  var nazev = seznamKytek[index][0];
-  var adresa = seznamKytek[index][1];
-  $(".reseni").removeClass("click");
-  $("#obrazek").attr("src", adresa);
-  $(".reseni-text").html(nazev);
-  index++;
-  $("#kytky-progress").css("width", index*(100/52) + "%");
-  if(index >= 52) {
-    seznamKytek = shuffle(seznamKytek);
-    index = 0;
-  }
-}
-function parseCSV() {
-  Papa.parse("kytky.csv", {
-    download: true,
-    encoding: "UTF-8",
-    delimiter: ";",
-    comments: "#",
-    complete: function (results) {
-      seznamKytek = results.data;
-      seznamKytek = shuffle(seznamKytek);
-      $("#pripravit-glyph").removeClass("glyphicon-circle-arrow-down");
-      $("#pripravit-glyph").addClass("glyphicon-ok");
+    var nazev = seznamKytek[index][0];
+    var adresa = seznamKytek[index][1];
+    $(".reseni").removeClass("click");
+    $("#obrazek").attr("src", adresa);
+    $(".reseni-text").html(nazev);
+    index++;
+    $("#kytky-progress").css("width", index * (100 / 52) + "%");
+    if (index >= 52) {
+        seznamKytek = shuffle(seznamKytek);
+        index = 0;
     }
-  });
 }
+
+function parseCSV() {
+    Papa.parse("kategorie.csv", {
+        download: true,
+        encoding: "UTF-8",
+        delimiter: ";",
+        comments: "#",
+        complete: function(results) {
+            var seznamKat = results.data;
+            $("#kat-list").empty();
+            seznamKat.forEach(function(kat) {
+                $("#kat-list").append('<li class="list-group-item kat-item active noselect"><i class="fa fa-lg fa-check-square-o" aria-hidden="true"></i> ' + kat[1] + '</li>');
+            });
+            $(".kat-item").click(function() {
+                $(this).toggleClass("active");
+                $("i", this).toggleClass("fa-square-o").toggleClass("fa-check-square-o")
+            });
+        }
+    });
+    Papa.parse("kytky.csv", {
+        download: true,
+        encoding: "UTF-8",
+        delimiter: ";",
+        comments: "#",
+        complete: function(results) {
+            seznamKytek = results.data;
+            seznamKytek = shuffle(seznamKytek);
+            $("#pripravit-glyph").removeClass("glyphicon-circle-arrow-down");
+            $("#pripravit-glyph").addClass("glyphicon-ok");
+        }
+    });
+}
+
 function shuffle(array) {
     let counter = array.length;
     while (counter > 0) {
@@ -38,12 +59,12 @@ function shuffle(array) {
     }
     return array;
 }
-$('.reseni').click(function () {
-  $(".reseni").toggleClass("click");
+$('.reseni').click(function() {
+    $(".reseni").toggleClass("click");
 });
-$( document ).on( "vclick", ".nahodna", function() {
-  nahodnyObrazek();
-  });
-$( document ).on( "vclick", ".pripravit", function() {
-  parseCSV();
-  });
+$(document).on("vclick", ".nahodna", function() {
+    nahodnyObrazek();
+});
+$(document).on("vclick", ".pripravit", function() {
+    parseCSV();
+});
