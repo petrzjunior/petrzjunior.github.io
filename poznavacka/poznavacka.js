57b017f1c62bbe540ee70082e180be5b4b18ed23
petrzjunior <junior@zahradnik.cz>
2016-12-09 16:25:12 +0100

    Nova poznavacka
    * New poznavacka design
    
    * Poznavacka design rework
    
    * Category selection system
    
    * Finishing Poznavacka beta 2.0
    
    * Remove jQuery mobile

--- poznavacka/poznavacka.js
+++ poznavacka/poznavacka.js
@@ -1,49 +1,35 @@
-﻿var seznamKytek;
-var index = 0;
-function nahodnyObrazek() {
-  var nazev = seznamKytek[index][0];
-  var adresa = seznamKytek[index][1];
-  $(".reseni").removeClass("click");
-  $("#obrazek").attr("src", adresa);
-  $(".reseni-text").html(nazev);
-  index++;
-  $("#kytky-progress").css("width", index*(100/52) + "%");
-  if(index >= 52) {
-    seznamKytek = shuffle(seznamKytek);
-    index = 0;
-  }
+var json = [];
+
+function parseJSON() {
+    $.getJSON('organismy.json', function(data) {
+        json = data;
+    });
+    json.forEach(function(kat) {
+        $("#kat-list").append('<li class="list-group-item kat-item ' + (kat.zapnuto ? 'active' : '') + ' noselect">' + kat.napis + '</li>');
+    });
+
 }
-function parseCSV() {
-  Papa.parse("kytky.csv", {
-    download: true,
-    encoding: "UTF-8",
-    delimiter: ";",
-    comments: "#",
-    complete: function (results) {
-      seznamKytek = results.data;
-      seznamKytek = shuffle(seznamKytek);
-      $("#pripravit-glyph").removeClass("glyphicon-circle-arrow-down");
-      $("#pripravit-glyph").addClass("glyphicon-ok");
-    }
-  });
+
+function dalsi() {
+    var kat = json[Math.floor((Math.random() * (json.length - 1)) + 0)];
+    var org = kat.organismy[Math.floor(Math.random() * kat.organismy.length)];
+    var jmeno = org.nazev;
+    var url = org.obrazky[Math.floor(Math.random() * org.obrazky.length)].url;
+    $(".reseni").removeClass("click");
+    $(".reseni-text").html(jmeno);
+    $("#obrazek").attr("src", url);
 }
-function shuffle(array) {
-    let counter = array.length;
-    while (counter > 0) {
-        let index = Math.floor(Math.random() * counter);
-        counter--;
-        let temp = array[counter];
-        array[counter] = array[index];
-        array[index] = temp;
-    }
-    return array;
-}
-$('.reseni').click(function () {
-  $(".reseni").toggleClass("click");
+$('.reseni').click(function() {
+    $(".reseni").toggleClass("click");
 });
-$( document ).on( "vclick", ".nahodna", function() {
-  nahodnyObrazek();
-  });
-$( document ).on( "vclick", ".pripravit", function() {
-  parseCSV();
-  });
+$(document).on("vclick", ".nahodna", function() {
+    nahodnyObrazek();
+});
+$(document).on("vclick", ".pripravit", function() {
+    parseCSV();
+});
+$(document).on("click", ".kat-item", function() {
+    $(this).toggleClass("active");
+    var on = $(this).hasClass("active");
+    // Dodělat zapnutí
+});
