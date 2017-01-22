d4be2024b8a45f58b13ee3d0816ba7596d07ddbc
Petr Zahradník <junior@zahradnik.cz>
2017-01-22 16:03:39 +0100

    Poznavacka 2.1

--- poznavacka/poznavacka.js
+++ poznavacka/poznavacka.js
@@ -1,36 +1,41 @@
 var json = [];
+var vybrane = [];
 
 function parseJSON() {
-    $.getJSON('organismy.json', function(data) {
-        json = data;
-    });
+  $.getJSON('organismy.json', function(data) {
+    vybrane = json = data;
     $("#kat-list").empty();
     json.forEach(function(kat) {
-        $("#kat-list").append('<li class="list-group-item kat-item ' + (kat.zapnuto ? 'active' : '') + ' noselect">' + kat.napis + '</li>');
+      $("#kat-list").append('<li class="list-group-item ' + (kat.zapnuto ? 'active' : '') + ' noselect" id="' + kat.nazev + '">' + kat.napis + '</li>');
     });
-
+  });
 }
 
 function dalsi() {
-    var kat = json[Math.floor((Math.random() * (json.length - 1)) + 0)];
-    var org = kat.organismy[Math.floor(Math.random() * kat.organismy.length)];
-    var jmeno = org.nazev;
-    var url = org.obrazky[Math.floor(Math.random() * org.obrazky.length)].url;
-    $(".reseni").removeClass("click");
-    $(".reseni-text").html(jmeno);
-    $("#obrazek").attr("src", url);
+  var kat = vybrane[Math.floor((Math.random() * (json.length - 1)) + 0)];
+  var org = kat.organismy[Math.floor(Math.random() * kat.organismy.length)];
+  var jmeno = org.nazev;
+  var url = org.obrazky[Math.floor(Math.random() * org.obrazky.length)].url;
+  $(".reseni").removeClass("click");
+  $(".reseni-text").html(jmeno);
+  $(".obrazek").attr("src", url);
 }
 $('.reseni').click(function() {
-    $(".reseni").toggleClass("click");
+  $(".reseni").toggleClass("click");
 });
-$(document).on("vclick", ".nahodna", function() {
-    nahodnyObrazek();
+$(document).on("click", ".list-group-item", function() {
+  vybrane = [];
+  $(this).toggleClass("active");
+  var on = $(this).hasClass("active");
+  for (var i = 0; i < json.length; i++) {
+    if(json[i].nazev == $(this).attr("id")) {
+      json[i].zapnuto = on;
+    }
+    if(json[i].zapnuto){
+      vybrane.push(json[i]);
+    }
+  }
 });
-$(document).on("vclick", ".pripravit", function() {
-    parseCSV();
-});
-$(document).on("click", ".kat-item", function() {
-    $(this).toggleClass("active");
-    var on = $(this).hasClass("active");
-    // Dodělat zapnutí
+$(document).ready(function() {
+  parseJSON();
 });
