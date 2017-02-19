1fb84d17c959ac3f17adb144cfba85eb90730388
Petr Zahradník <junior@zahradnik.cz>
2017-02-19 15:50:10 +0100

    Poznavacka now Angular!

--- poznavacka/js/app.js
+++ poznavacka/js/app.js
@@ -1,1 +1,36 @@
-// JavaScript source code
+var poznavackaApp = angular.module("poznavackaApp", ["ngRoute"]);
+
+poznavackaApp.config(['$routeProvider',
+    function ($routeProvider) {
+        $routeProvider.
+            when('/poznavacka', {
+                templateUrl: '/html/poznavacka.html',
+                controller: 'poznavackaCtrl'
+            }).
+            when('/galerie', {
+                templateUrl: '/html/galerie.html',
+                controller: 'galerieCtrl'
+            }).
+            otherwise({
+                redirectTo: '/poznavacka'
+            })
+    }
+]);
+
+poznavackaApp.factory("loaderService", function (poznavackaConfig) {
+      return poznavackaConfig.setList;
+});
+
+poznavackaApp.constant("poznavackaConfig", {
+    setList: [
+      {name: "mekkysi", desc: "Měkkýši (Pracuje se na tom)", enabled: false, file: "clenovci.json"},
+      {name: "clenovci", desc: "Členovci", enabled: true, file: "clenovci.json"}
+    ]
+});
+
+poznavackaApp.controller("mainCtrl", function($scope, $location) {
+  $scope.isActive = function (viewLocation) {
+       var active = (viewLocation === $location.path());
+       return active;
+  };
+});
---
+++ poznavacka/js/galerie.js
@@ -0,0 +1,27 @@
+poznavackaApp.controller("galerieCtrl", function($scope, $http, loaderService) {
+  $scope.setList = [];
+  $scope.imgList = [];
+
+  $scope.updateSelected = function() {
+    $scope.imgList = [];
+    for(var index = 0; index < $scope.setList.length; index++) {
+      if($scope.setList[index].enabled) {
+        $http.get($scope.setList[index].file).then(function(data) {
+          $scope.imgList = $scope.imgList.concat(data.data);
+        });
+      }
+    }
+  };
+
+  var loadSets = function(){
+    $scope.setList = loaderService;
+  };
+  loadSets();
+  $scope.updateSelected();
+
+  $scope.next = function() {
+    $scope.showSolution = false;
+    $scope.thisOne = $scope.imgList[Math.floor((Math.random() * ($scope.imgList.length - 1)) + 0)];
+    console.log($scope.thisOne);
+  };
+});
