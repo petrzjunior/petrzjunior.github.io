8731ebd30811977c8c309ab00aed217e29aa982a
Petr Zahradník <junior@zahradnik.cz>
2017-03-11 11:52:53 +0100

    Poznavacka refactoring

--- poznavacka/js/app.js
+++ poznavacka/js/app.js
@@ -1,39 +1,39 @@
 var poznavackaApp = angular.module("poznavackaApp", ["ngRoute"]);
 
 poznavackaApp.config(['$routeProvider',
-    function ($routeProvider) {
+    function($routeProvider) {
         $routeProvider.
-            when('/poznavacka', {
-                templateUrl: 'html/poznavacka.html',
-                controller: 'poznavackaCtrl'
-            }).
-            when('/galerie', {
-                templateUrl: 'html/galerie.html',
-                controller: 'galerieCtrl'
-            }).
-            otherwise({
-                redirectTo: 'poznavacka'
-            })
+        when('/poznavacka', {
+            templateUrl: 'html/poznavacka.html',
+            controller: 'poznavackaCtrl'
+        }).
+        when('/galerie', {
+            templateUrl: 'html/galerie.html',
+            controller: 'galerieCtrl'
+        }).
+        otherwise({
+            redirectTo: 'poznavacka'
+        })
     }
 ]);
 
-poznavackaApp.factory("loaderService", function (poznavackaConfig) {
-      return poznavackaConfig.setList;
+poznavackaApp.factory("loaderService", function(poznavackaConfig) {
+    return poznavackaConfig.setList;
 });
 
 poznavackaApp.constant("poznavackaConfig", {
     setList: [
-      {name: "mekkysi", desc: "Měkkýši (Pracuje se na tom)", enabled: false, file: "clenovci.json"},
-      {name: "clenovci1", desc: "Členovci 1. část", enabled: false, file: "clenovci1.json"},
-      {name: "clenovci2", desc: "Členovci 2. část", enabled: false, file: "clenovci2.json"},
-      {name: "clenovci3", desc: "Členovci 3. část", enabled: false, file: "clenovci3.json"},
-      {name: "clenovci", desc: "Členovci vše", enabled: true, file: "clenovci.json"}
+        { name: "mekkysi", desc: "Měkkýši (Pracuje se na tom)", enabled: false, file: "clenovci.json" },
+        { name: "clenovci1", desc: "Členovci 1. část", enabled: false, file: "clenovci1.json" },
+        { name: "clenovci2", desc: "Členovci 2. část", enabled: false, file: "clenovci2.json" },
+        { name: "clenovci3", desc: "Členovci 3. část", enabled: false, file: "clenovci3.json" },
+        { name: "clenovci", desc: "Členovci vše", enabled: true, file: "clenovci.json" }
     ]
 });
 
 poznavackaApp.controller("mainCtrl", function($scope, $location) {
-  $scope.isActive = function (viewLocation) {
-       var active = (viewLocation === $location.path());
-       return active;
-  };
+    $scope.isActive = function(viewLocation) {
+        var active = (viewLocation === $location.path());
+        return active;
+    };
 });
--- poznavacka/js/poznavacka.js
+++ poznavacka/js/poznavacka.js
@@ -1,33 +1,33 @@
 poznavackaApp.controller("poznavackaCtrl", function($scope, $http, loaderService) {
-  $scope.setList = [];
-  $scope.imgList = [];
-
-  $scope.updateSelected = function() {
+    $scope.setList = [];
     $scope.imgList = [];
-    for(var index = 0; index < $scope.setList.length; index++) {
-      if($scope.setList[index].enabled) {
-        $http.get($scope.setList[index].file).then(function(data) {
-          $scope.imgList = $scope.imgList.concat(data.data);
-        });
-      }
-    }
-  };
 
-  var loadSets = function(){
-    $scope.setList = loaderService;
-  };
-  loadSets();
-  $scope.updateSelected();
+    $scope.updateSelected = function() {
+        $scope.imgList = [];
+        for (var index = 0; index < $scope.setList.length; index++) {
+            if ($scope.setList[index].enabled) {
+                $http.get($scope.setList[index].file).then(function(data) {
+                    $scope.imgList = $scope.imgList.concat(data.data);
+                });
+            }
+        }
+    };
 
-  $scope.next = function() {
-    $scope.showSolution = false;
-    $scope.thisOne = $scope.imgList[Math.floor((Math.random() * ($scope.imgList.length - 1)) + 0)];
-    console.log($scope.thisOne);
-  };
+    var loadSets = function() {
+        $scope.setList = loaderService;
+    };
+    loadSets();
+    $scope.updateSelected();
+
+    $scope.next = function() {
+        $scope.showSolution = false;
+        $scope.thisOne = $scope.imgList[Math.floor((Math.random() * ($scope.imgList.length - 1)) + 0)];
+        console.log($scope.thisOne);
+    };
 });
 
-poznavackaApp.filter('default', [function(){
-  return function(value, def) {
-    return value || def;
-  };
+poznavackaApp.filter('default', [function() {
+    return function(value, def) {
+        return value || def;
+    };
 }]);
